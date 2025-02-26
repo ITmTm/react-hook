81c77962efc504ba5e56de26d2ab9995c8371e44
mtm <Stafr95@mail.ru>
2025-02-26 17:31:53 +0500

    Using useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 
@@ -47,6 +47,10 @@ import './App.css';
 //     }
 // }
 
+const countTotal = (num) => {
+	console.log('counting...');
+	return num + 10;
+}
 
 const Slider = (props) => {
     
@@ -73,23 +77,20 @@ const Slider = (props) => {
     function toggleAutoplay() {
         setAutoplay(autoplay => !autoplay)
     }
+
+	const total = useMemo(() => {
+		return countTotal(slide)
+	}, []);
     
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
 
-			{/*	{
-					getSomeImages().map((url, i) => {
-						return (
-							<img key={i} className="d-block w-100" src={url} alt="slide"/>
-						)
-					})
-				}*/}
-
 				<Slide getSomeImages={getSomeImages} />
 
-				<div className="text-center mt-5">Active slide { slide } <br/> { autoplay ? 'auto' : null }</div>
+				<div className="text-center mt-5">Active slide {slide} <br/> { autoplay ? 'auto' : null }</div>
+				<div className="text-center mt-5">Total slides {total}</div>
 				<div className="buttons mt-3 d-flex justify-content-center">
                     <button
                         className="btn btn-primary me-2"
